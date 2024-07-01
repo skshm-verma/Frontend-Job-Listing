b1b640fd8e5dfbb0fba8c33a2f3dfce61b9f8cb3
Saksham <skshm.090898@gmail.com>
2024-07-01 17:01:11 +0530

    few changes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -51,54 +51,54 @@ const LoginPage = ({ setCurrentUser }) => {
         <div className='flex'>
             <Toaster position="top-center" reverseOrder={false} />
             <div className='flex justify-center items-center w-screen h-screen lg:bg-white bg-[#FFEFEF]'>
-            <div className='lg:w-[60%] w-full flex justify-center items-center '>
-                <div className='flex flex-col md:items-start items-center gap-2'>
-                    <div className='flex flex-col justify-center items-center md:justify-center md:items-start md:gap-2 gap-1'>
-                    <h1 className='tracking-normal text-black font-semibold md:text-5xl text-3xl py-1 font-dm md:mx-0 mx-auto'>Already have an account?</h1>
-                    <h2 className='tracking-tight text-[#525252] md:text-2xl text-xl pb-8 md:mx-0 mx-auto'>Your personal job finder is here</h2>
-                    </div>
-                    <div className='inline-block border-[#C2C2C2] pb-2'>
-                        <input
-                            type="email"
-                            placeholder='Email'
-                            value={email}
-                            onInput={(e) => setEmail(e.target.value)}
-                            className='border mx-1 py-2 px-4 rounded-md md:w-[22rem] w-full focus:outline-none focus:ring-1 focus:ring-[#ED5353]'
-                            onKeyDown={handleKeyDown}
-                        />
-                        {errors.email && <div className='text-sm mx-2 my-1 text-[#ED5353]'>{errors.email}</div>}
-                    </div>
-                    <div className='inline-block border-[#C2C2C2] pb-2'>
-                        <form onKeyDown={handleKeyDown}>
+                <div className='lg:w-[60%] w-full flex justify-center items-center '>
+                    <div className='flex flex-col md:items-start items-center gap-2'>
+                        <div className='flex flex-col justify-center items-center md:justify-center md:items-start md:gap-2 gap-1'>
+                            <h1 className='tracking-normal text-black font-semibold md:text-5xl text-2xl py-1 font-dm'>Already have an account?</h1>
+                            <h2 className='tracking-tight text-[#525252] md:text-2xl text-xl pb-8'>Your personal job finder is here</h2>
+                        </div>
+                        <div className='inline-block border-[#C2C2C2] pb-2'>
                             <input
-                                type="password"
-                                placeholder='Password'
-                                value={password}
-                                onInput={(e) => setPassword(e.target.value)}
+                                type="email"
+                                placeholder='Email'
+                                value={email}
+                                onInput={(e) => setEmail(e.target.value)}
                                 className='border mx-1 py-2 px-4 rounded-md md:w-[22rem] w-full focus:outline-none focus:ring-1 focus:ring-[#ED5353]'
-
+                                onKeyDown={handleKeyDown}
                             />
-                        </form>
-                        {errors.password && <div className='text-sm mx-2 my-1 text-[#ED5353]'>{errors.password}</div>}
-                    </div>
-                    <button
-                        onClick={handleLogin}
-                        className='mb-2 block tracking-wider bg-[#ED5353] font-dm font-xl text-white mx-2 py-2 px-8 rounded-md hover:bg-[#f66565] duration-200 mx-auto md:mx-0'
-                    >Sign In</button>
-                    <div className='md:mx-2 mx-auto'>
-                        <span className='tracking-tight text-[#525252]'>Don’t have an account?</span>
+                            {errors.email && <div className='text-sm mx-2 my-1 text-[#ED5353]'>{errors.email}</div>}
+                        </div>
+                        <div className='inline-block border-[#C2C2C2] pb-2'>
+                            <form onKeyDown={handleKeyDown}>
+                                <input
+                                    type="password"
+                                    placeholder='Password'
+                                    value={password}
+                                    onInput={(e) => setPassword(e.target.value)}
+                                    className='border mx-1 py-2 px-4 rounded-md md:w-[22rem] w-full focus:outline-none focus:ring-1 focus:ring-[#ED5353]'
+
+                                />
+                            </form>
+                            {errors.password && <div className='text-sm mx-2 my-1 text-[#ED5353]'>{errors.password}</div>}
+                        </div>
                         <button
-                            onClick={() => { navigate('/register') }}
-                            className='px-2 font-semibold underline underline-offset-2 hover:text-[#ED5353] duration-300'>Sign Up</button>
+                            onClick={handleLogin}
+                            className='mb-2 block tracking-wider bg-[#ED5353] font-dm font-xl text-white mx-2 py-2 px-8 rounded-md hover:bg-[#f66565] duration-200 mx-auto md:mx-0'
+                        >Sign In</button>
+                        <div className='md:mx-2 mx-auto'>
+                            <span className='tracking-tight text-[#525252]'>Don’t have an account?</span>
+                            <button
+                                onClick={() => { navigate('/register') }}
+                                className='px-2 font-semibold underline underline-offset-2 hover:text-[#ED5353] duration-300'>Sign Up</button>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
             <div className='w-[40%] relative lg:block hidden'>
                 <div className='absolute w-full flex z-20 justify-center items-center top-10'>
                     <span className='text-white text-4xl font-dm'>Your Personal Job Finder</span>
                 </div>
-                <img className='w-full h-screen'src={image1} alt="LandingPageImage" />
+                <img className='w-full h-screen' src={image1} alt="LandingPageImage" />
             </div>
             {redirectToHome && <Navigate to="/" />}
         </div>
